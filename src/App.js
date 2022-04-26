4ec8a62f93233182aad1bbf45a18430d2ff1eac6
ARUN BASADA <abasada@tenna.com>
2022-04-26 10:32:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import Players from "./components/players/Players";
+import ViewPlayer from "./components/viewPlayer/ViewPlayer";
+import CreatePlayer from "./components/createPlayer.js/CreatePlayer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/player" component={Players} />
+          <Route exact path="/player/:id" component={ViewPlayer} />
+          <Route exact path="/player/createplayer" component={CreatePlayer} />
+        </Switch>
+      </Router>
     </div>
   );
 }
